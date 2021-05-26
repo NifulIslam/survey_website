b905e6498cfc3b6b6fd945813053fdb559432d13
Niful Islam <78269383+NifulIslam@users.noreply.github.com>
2021-05-26 21:53:08 +0600

    Add files via upload

---
+++ src/addQuestionSet.php
@@ -0,0 +1,29 @@
+<?php
+if( !isset($_REQUEST["t_a"]) ){
+    header("Location: targated_aud.php"); 
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>add question</title>
+    </head>
+    <body>
+        <a href= "researcher_profile.php" > home </a>
+
+        <form action="add_mcq.php" method="get">
+        <input type = "submit" name="addMcq" value ="add mcq">
+        <br>
+        </form>
+        <form action="add_cq.php" method="get">
+        <input type = "submit" name="addCq" value ="add cq">
+        </form>
+    </body>
+    </html>
---
+++ src/history_details.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(isset($_SESSION["r_id"]) && $_SESSION["type"] == "researcher" && isset($_REQUEST["q_s"])){
+    $q_s= $_REQUEST["q_s"];
+    $query= "SELECT qus.question AS question, qus.question_id AS question_id ,qus.ismcq AS is_mcq FROM responce AS rsp JOIN questions AS qus WHERE qus.q_s_id=$q_s GROUP BY qus.question_id";
+    $connection = mysqli_connect("localhost", "root", "", "ask_me_db");
+    $result= mysqli_query($connection, $query);
+    while($data= mysqli_fetch_assoc($result)){
+        if($data["is_mcq"] ==1){
+            echo explode("??",$data["question"])[0]."\t";
+        }
+        else{echo $data["question"]."\t";}
+        $q_id= $data["question_id"];
+        echo "<a href='check_responce.php?q_id=$q_id'> check responce <a>";
+        echo "<br>";
+        
+    }
+
+
+
+}
+else{ header("Location: researcher_profile.php");}
+
+?>
+<!DOCTYPE html><html lang="en"><head><title> question set history</title></head><body></body></html>
